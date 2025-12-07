b29c703f6a858034b25e4a0cb18f81cf5d28d7da
Bryan Honof <bryanhonof@gmail.com>
2025-12-08 00:43:42 +0100

    ratatouille: init at 0.9.11

---
+++ pkgs/ratatouille/default.nix
@@ -0,0 +1,63 @@
+{
+  lib,
+  stdenv,
+  fetchFromGitHub,
+  autoPatchelfHook,
+
+  withPortaudio ? false,
+  withJack ? true,
+
+  pkg-config,
+  libsndfile,
+  cairo,
+  libx11,
+  lv2,
+  jack2,
+  portaudio,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "ratatouille.lv2";
+  version = "0.9.11";
+
+  src = fetchFromGitHub {
+    owner = "brummer10";
+    repo = "Ratatouille.lv2";
+    fetchSubmodules = true;
+    rev = "v${finalAttrs.version}";
+    hash = "sha256-mig3yUGSNz1xuyz6ljKqJUjNqmEcsbXSH1vTxTGdOFk=";
+  };
+
+  postPatch = ''
+    substituteInPlace Ratatouille/makefile \
+      --replace-fail "-flto=auto" ""
+  '';
+
+  makeFlags = [
+    "INSTALL_DIR=${placeholder "out"}/lib/lv2"
+    "EXE_INSTALL_DIR=${placeholder "out"}/bin"
+    "CLAP_INSTAL_DIR=${placeholder "out"}/lib/clap"
+    "VST2_INSTAL_DIR=${placeholder "out"}/lib/vst2"
+  ];
+
+  dontStrip = true;
+
+  nativeBuildInptus = [
+    autoPatchelfHook
+  ];
+
+  buildInputs = [
+    pkg-config
+    libsndfile
+    cairo
+    libx11
+    lv2
+    cairo
+  ]
+  ++ lib.optionals withPortaudio [
+    portaudio
+  ]
+  ++ lib.optionals withJack [
+    jack2
+  ];
+})
